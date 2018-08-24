96dc69a4d182561a323aa1fd8e389f1df842e557
Yurii-Adamenko <adamyura97@gmail.com>
2018-08-24 04:50:12 +0300

    Часть 2 Занятие #16

--- js/main.js
+++ js/main.js
@@ -1,77 +1,101 @@
+// пункт 1
+console.log('1)');
+
+function outputLine (myString) {
+  if (myString) {
+    console.log(myString);
+  }
+  else {
+    console.log('The output string is empty! Please enter a string.');
+  }
+  return myString;
+}
+
+outputLine('Hello world!');
+outputLine('');
+
 // пункт №2
 console.log('2)');
 
-var myString = 'Hello',
-  myNumber = 10,
-  myBool = true,
-  block = [1, 2, 3, 4, 5],
-  obj = {lang: 'JS'};
+var browser = {
+  name: 'Chrome',
+  created: 2008,
+  version: '68.0.3440.106'
+}
 
-console.log(myNumber);
-console.log(myString);
-console.log(myBool);
-console.log(block);
-console.log(obj);
+console.log(browser);
+console.log(browser.name);
+console.log(browser.created);
+console.log(browser.version);
 
-// пункт 3
+// пункт №3
 console.log('3)');
 
-myNumber = myNumber.toFixed(2);
-console.log(myNumber);
+browser.myMethod = function() {
+  return browser.name + ' is one of the most popular browsers in the world.';
+}
+
+console.log(browser);
+console.log(browser.myMethod());
 
 // пункт №4
 console.log('4)');
 
-var i = 5;
-console.log('i = ' + i);
-console.log(++i);
-console.log(i++);
-console.log(i);
+for (var i = 1; i < 21; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
 
 // пункт №5
 console.log('5)');
 
-var n = 10;
-console.log('n = ' + n);
-n += 15;
-console.log('n = n + 15 = ' + n);
-n -= 5;
-console.log('n = n - 5 = ' + n);
-n *= 2;
-console.log('n = n * 2 = ' + n);
-n /= 4;
-console.log('n = n / 4 = ' + n);
-n %= 3;
-console.log('n = n % 3 = ' + n);
+function typeNumber (myNumber) {
+  switch(true) {
+    case myNumber > 0:
+      console.log(myNumber + ' - this is a positive number'); break;
+    case myNumber < 0:
+      console.log(myNumber + ' - this is a negative number'); break; 
+    case myNumber === 0:
+      console.log(myNumber + ' - it is zero'); break;  
+    default:
+      console.log(myNumber + ' - this is not a number. Please enter a number');
+  }
+}
+
+typeNumber(10);
+typeNumber(-10);
+typeNumber(0);
+typeNumber('hello');
 
 // пункт №6
 console.log('6)');
 
-console.log(Math.PI);
-console.log(Math.round(89.279));
-console.log(Math.random()*10);
-console.log(Math.pow(2,5));
+function howManyYears (age) {
+  age < 1997 ? console.log('more...'): age > 1997 ? console.log('less...'): console.log('exactly!');
+}
+
+howManyYears(1800);
+howManyYears(2100);
+howManyYears(1997);
 
 // пункт №7
 console.log('7)');
 
-var myString2 = 'Hello world!';
-console.log('length of the string: "%s" is ' + myString2.length, myString2);
+var z = 0;
+while (z <= 7) {
+  console.log(z++);
+}
 
 // пункт №8
 console.log('8)');
 
-console.log(myString2.indexOf('world'));
+function exponentiation (arg1, arg2, callback) {
+  console.log('Число ' + arg1 + ' в степени ' + arg2);
+  var arg3 = Math.pow(arg1,arg2);
+  callback(arg3);
+}
 
-// пункт №9
-console.log('9)');
-
-console.log(myString2.replace('world', 'Andrey'));
-
-// пункт №10
-console.log('10)');
-
-myString2 = myString2.toUpperCase();
-console.log(myString2);
-myString2 = myString2.toLowerCase();
-console.log(myString2);
+exponentiation(2, 4, function(result) {
+  console.log('Результат ' + result);
+});
